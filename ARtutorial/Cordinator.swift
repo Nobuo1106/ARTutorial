ba7ca29da680715a2d1a3772c84ff20366261d03
initial commit <>
2022-10-08 17:46:33 +0900

    タップ時に一つの3Dモデルのみ表示するように修正

--- ARtutorial/Cordinator.swift
+++ ARtutorial/Cordinator.swift
@@ -18,14 +18,16 @@ class Coordinator: NSObject {
     @objc func handleTap(_ recognizer: UITapGestureRecognizer) {
         print("updateUIView")
         guard let view = self.view else { return }
+        
+        guard view.scene.anchors.first (where: { $0.name == "LunarRoverAnchor" }) == nil else {
+            return
+        }
         let tapLocation = recognizer.location(in: view)
         let results = view.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal)
         if let result = results.first {
             let anchor = AnchorEntity(raycastResult: result)
             
-            cancellable = ModelEntity.loadAsync(named: "shoe")
-                .append(ModelEntity.loadAsync(named: "teapot"))
-                .collect()
+            cancellable = ModelEntity.loadAsync(named: "LunarRover")
                 .sink{ loadCompletion in
                     if case let .failure(error) = loadCompletion {
                         print("Unable to load model \(error)")
@@ -33,15 +35,12 @@ class Coordinator: NSObject {
                     
                     self.cancellable?.cancel()
                     
-                } receiveValue: { entities in
-                    var x: Float = 0.0
-                    entities.forEach { entity in
-                        entity.position = simd_make_float3(x,0,0)
-                        anchor.addChild(entity)
-                        x += 0.3
-                    }
+                } receiveValue: { entity in
+                    anchor.name = "LunarRoverAnchor"
+                    anchor.addChild(entity)
                 }
             view.scene.addAnchor(anchor)
         }
     }
 }
+
